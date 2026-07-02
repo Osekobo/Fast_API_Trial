fb9cb6f7eefab53d4518e0bb19964dcc7c73fd25
Oseko <collinsoseko2003@gmail.com>
2026-07-02 20:10:47 +0300

    Path parameters containing paths

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ def read_user_me():
 @app.get("/users/{user_id}")
 def read_item(user_id: str):
     return {"Item id": user_id}
+
+
+@app.get("/files/{file_path:path}")
+def read_file(file_path: str):
+    return {"file_path": file_path}
