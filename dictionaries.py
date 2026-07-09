6d171a2c685d7d56784c0d645570ed02e88116df
Oseko <collinsoseko2003@gmail.com>
2026-07-09 21:43:52 +0300

    New trial

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+# Python Dictionary methods
+data = {1: {"name": "13kg Gas Refill", "price": 2500.0, "is_stock": True},
+        2: {"name": "6kg Gas Refill", "price": 1200.0, "is_stock": True}
+        }
+print(data.items())
